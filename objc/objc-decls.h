cea1c26f43592389bd7d0298a327371642204687
nico <nico@72102866-910b-0410-8b05-ffd578937521>
2002-04-18 10:29:52 +0000

    Updated to latest gnustep-make conventions on Windows DLL export/import
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/libobjc/trunk@13503 72102866-910b-0410-8b05-ffd578937521

--- objc/objc-decls.h
+++ objc/objc-decls.h
@@ -3,15 +3,21 @@
 #ifndef __objc_decls_H__
 #define __objc_decls_H__
 
+#ifdef GNUSTEP_WITH_DLL
+
 #if BUILD_libobjc_DLL
 #  define objc_EXPORT  __declspec(dllexport)
 #  define objc_DECLARE __declspec(dllexport)
-#elif libobjc_ISDLL
+#else
 #  define objc_EXPORT  extern __declspec(dllimport)
 #  define objc_DECLARE extern __declspec(dllimport)
+#endif
+
 #else
+
 #  define objc_EXPORT  extern
 #  define objc_DECLARE 
+
 #endif
 
 #endif /* __objc_decls_H__ */
