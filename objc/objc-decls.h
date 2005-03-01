6c1640e44d1bce3e8400ffb29e7c60e06f836108
nico <nico@72102866-910b-0410-8b05-ffd578937521>
2005-03-01 04:57:45 +0000

    Updates for the new building code on MinGW
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/libobjc/trunk@20818 72102866-910b-0410-8b05-ffd578937521

--- objc/objc-decls.h
+++ objc/objc-decls.h
@@ -6,8 +6,17 @@
 #ifdef GNUSTEP_WITH_DLL
 
 #if BUILD_libobjc_DLL
+#
+# if defined(__MINGW32__)
+  /* On Mingw, the compiler will export all symbols automatically, so
+   * __declspec(dllexport) is not needed.
+   */
+#  define objc_EXPORT  extern
+#  define objc_DECLARE 
+# else
 #  define objc_EXPORT  __declspec(dllexport)
 #  define objc_DECLARE __declspec(dllexport)
+# endif
 #else
 #  define objc_EXPORT  extern __declspec(dllimport)
 #  define objc_DECLARE extern __declspec(dllimport)
