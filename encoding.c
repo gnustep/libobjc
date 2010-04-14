3a791cca56be77b8918146921b0dfe270408a1e3
rfm <rfm@72102866-910b-0410-8b05-ffd578937521>
2010-04-14 10:36:15 +0000

    Fix objc_skip_offset() to avoid skipping and extra character
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/libobjc/trunk@30149 72102866-910b-0410-8b05-ffd578937521

--- encoding.c
+++ encoding.c
@@ -698,8 +698,8 @@ objc_skip_offset (const char *type)
 {
   if (*type == '+')
     type++;
-  while (isdigit ((unsigned char) *++type))
-    ;
+  while (isdigit ((unsigned char) *type))
+    type++;
   return type;
 }
 
